a06a3b68c32d5baa50fafc185f0d56ddf7dc121f
masterUNG <phrombutr@gmail.com>
2018-02-20 12:01:28 +0700

    Add Fragment Success

--- app/src/main/java/masterung/snru/ac/th/emercall/MainActivity.java
+++ app/src/main/java/masterung/snru/ac/th/emercall/MainActivity.java
@@ -3,11 +3,24 @@ package masterung.snru.ac.th.emercall;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import masterung.snru.ac.th.emercall.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+//        Add Fragment
+        if (savedInstanceState == null) {
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentMainFragment, new MainFragment()).commit();
+
+        }
+
+
+    }   // Main Method
+
+}   // Main Class
