1221bb19dcf837fbf052692e5252571b1ba84f0c
masterUNG <phrombutr@gmail.com>
2018-02-21 16:24:09 +0700

    Police Station Finish

--- app/src/main/java/masterung/snru/ac/th/emercall/fragment/MainFragment.java
+++ app/src/main/java/masterung/snru/ac/th/emercall/fragment/MainFragment.java
@@ -54,12 +54,54 @@ public class MainFragment extends Fragment {
 
 
         //        For Station 2
+        ImageView station2ImageView = getView().findViewById(R.id.imageViewStation2);
+        station2ImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                callStation("2222");
+            }
+        });
+        TextView station2TextView = getView().findViewById(R.id.textViewStation2);
+        station2TextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                callStation("2222");
+            }
+        });
 
 
         //        For Station 3
-
+        ImageView station3ImageView = getView().findViewById(R.id.imageViewStation3);
+        station2ImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                callStation("3333");
+            }
+        });
+        TextView station3TextView = getView().findViewById(R.id.textViewStation3);
+        station2TextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                callStation("3333");
+            }
+        });
 
         //        For Station 4
+        ImageView station4ImageView = getView().findViewById(R.id.imageViewStation4);
+        station2ImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                callStation("4444");
+            }
+        });
+        TextView station4TextView = getView().findViewById(R.id.textViewStation4);
+        station2TextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                callStation("4444");
+            }
+        });
+
 
 
     }   //Main Method
